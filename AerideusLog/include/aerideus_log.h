77c46ce09ed0a67edd69e0018fe0c13537a0b572
Rasmus Hugosson <rasmus.hugosson01@gmail.com>
2023-03-20 18:24:00 +0100

    Add severity levels

--- AerideusLog/include/aerideus_log.h
+++ AerideusLog/include/aerideus_log.h
@@ -2,3 +2,24 @@
 /*
 	Author @rasmushugosson
 */
+
+// Severity level -----------------------------------------------------------------------------------------------
+
+/// <summary>
+/// Used to specifies severity level of Aerides log messages.
+/// </summary>
+typedef enum {
+	TRACE = 0, INFO, WARNING, ERROR, FATAL
+} log_level;
+
+/// <summary>
+/// Sets the minimum severity required for a console messages to be printed.
+/// </summary>
+/// <param name="min">is the minimum log_level that will be printed</param>
+void ae_log_console_level_set(log_level min);
+
+/// <summary>
+/// Sets the minimum severity required for a file messages to be logged.
+/// </summary>
+/// <param name="min">is the minimum log_level that will be logged</param>
+void ae_log_file_level_set(log_level min);
---
+++ AerideusLog/src/ae_console.c
@@ -0,0 +1,8 @@
+#include "../include/aerideus_log.h"
+
+static log_level s_level = TRACE;
+
+void ae_log_console_level_set(log_level min)
+{
+	s_level = min;
+}
